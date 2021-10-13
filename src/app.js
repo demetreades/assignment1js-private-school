75fa57cbcbcc3689ad49e4a4c977c95a89038f2c
demetreades <demetreades@protonmail.com>
2021-10-13 22:45:54 +0300

    privateSchool import, getAll added

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ import {
   Course,
   Assignment,
   Subject,
+  privateSchool,
 } from '../src/models/index.js';
 import DateUtil from '../src/models/utils/DateUtil.js';
+
+// console.log(privateSchool);
 
 const {
   students: [panos, nikos, petros],
@@ -28,6 +31,9 @@ console.log(
 
 //
 //
+
+// /////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////
 
 // students variables
 const student1 = new Student(panos);
@@ -51,9 +57,6 @@ const subject2 = new Subject(css3);
 const subject3 = new Subject(oop);
 const subject4 = new Subject(security);
 
-// /////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////
-
 // Assignments variables
 const assignment1Private = new Assignment(assignment1);
 assignment1Private.setSubDateTime('19:59:00 11/11/2021');
@@ -62,28 +65,33 @@ const assignment3Private = new Assignment(assignment3);
 const assignment4Hotel = new Assignment(assignment4);
 const assignmentGroupProject = new Assignment(groupProject);
 
-// Course JS ///////////////////////////////////////////
+// Init Course JS ///////////////////////////////////////////
 
 // set end date
 courseJS.setEndDate('2021/12/14');
-// add subjects
+// add subjects to course
 courseJS.add('subjects', subject1);
 courseJS.add('subjects', subject2);
 courseJS.add('subjects', subject3);
 courseJS.add('subjects', subject4);
-// add students
+// add students to course
 courseJS.add('students', student1);
 courseJS.add('students', student2);
 courseJS.add('students', student3);
-// add trainers
+// add trainers to course
 courseJS.add('trainers', trainer1);
 courseJS.add('trainers', trainer2);
 courseJS.add('trainers', trainer3);
+// add assignments to course
+courseJS.add('assignments', assignment1Private);
+courseJS.add('assignments', assignment2Private);
+courseJS.add('assignments', assignment3Private);
+courseJS.add('assignments', assignment4Hotel);
 
 // remove students
 // courseJS.remove('students', student1);
 // courseJS.remove('students', student2);
-// courseJS.remove('students', student3);
+courseJS.remove('students', student3);
 
 // Student /////////////////////////////////////////////
 student1.add('courses', courseJS);
@@ -95,17 +103,19 @@ student1.submitAssignment(assignment1);
 student1.submitAssignment(assignment3);
 
 // Trainer /////////////////////////////////////////////
-trainer1.add('courses', courseJS);
 trainer1.add('subjects', subject1);
 trainer1.add('subjects', subject2);
 trainer1.add('subjects', subject3);
 trainer1.add('subjects', subject4);
-// trainer1.list('subjects', `Subject of trainer`);
+// trainer1.getAll('subjects', `Subject of trainer`);
 
 trainer1.giveMarks(student1, 0, 70, 80);
-// student1.list('assignments', 'Marked Assignment');
-courseJS.list('students', 'Students lista');
-console.table([student1]);
+// student1.getAll('assignments', 'Marked Assignment');
+// courseJS.getAll('students', 'Course JS Students: ');
+courseJS.getAll('trainers', 'Trainers: ');
+courseJS.getAll('students', 'Students: ');
+courseJS.getAll('assignments', 'Assignments: ');
+
 //
 //
 
