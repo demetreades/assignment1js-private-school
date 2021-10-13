1ebdd8317fca5c9289618af190ed26949b4dbd66
demetreades <demetreades@protonmail.com>
2021-10-13 22:46:23 +0300

    privateSchool import added

--- src/models/index.js
+++ src/models/index.js
@@ -3,5 +3,6 @@ import Trainer from './Trainer.js';
 import Course from './Course.js';
 import Assignment from './Assignment.js';
 import Subject from './Subject.js';
+import privateSchool from './School.js';
 
-export { Student, Trainer, Course, Assignment, Subject };
+export { Student, Trainer, Course, Assignment, Subject, privateSchool };
