1fa21ad963f76dc84ca001c828e7d27c200a45e5
demetreades <demetreades@protonmail.com>
2021-10-14 00:58:24 +0300

    eslint cycle dependencies

--- src/models/index.js
+++
@@ -1,8 +0,0 @@
-import Student from './Student.js';
-import Trainer from './Trainer.js';
-import Course from './Course.js';
-import Assignment from './Assignment.js';
-import Subject from './Subject.js';
-import privateSchool from './School.js';
-
-export { Student, Trainer, Course, Assignment, Subject, privateSchool };
