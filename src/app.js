2fb430df47e2449457e7e58fe84041142c8902d9
demetreades <demetreades@protonmail.com>
2021-10-13 23:42:35 +0300

    privateSchool imported

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
-import DATA from '../src/config/DATA.js';
-import {
-  Student,
-  Trainer,
-  Course,
-  Assignment,
-  Subject,
-  privateSchool,
-} from '../src/models/index.js';
+import { privateSchool } from '../src/models/index.js';
 import DateUtil from '../src/models/utils/DateUtil.js';
 
-// console.log(privateSchool);
-
 const {
-  students: [panos, nikos, petros],
-  trainers: [mantoudis, mpozas, xlis],
-  courses: [cb13Java, cb13Python, cb13JS, cb13Csharp],
-  subjects: [html5, css3, oop, security],
+  students: [student1, student2, student3, student4],
+  trainers: [trainer1, trainer2, trainer3],
+  courses: [courseJava, coursePython, courseJS, courseCsharp],
+  subjects: [subject1, subject2, subject3, subject4, subject5],
   assignments: [
     assignment1,
     assignment2,
@@ -23,51 +13,16 @@ const {
     assignment4,
     groupProject,
   ],
-} = DATA;
-
-console.log(
-  `\n\n[${DateUtil.today}] PROGRAM =========== ============ START \n`
-);
-
-//
-//
+} = privateSchool;
 
 // /////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////
 
-// students variables
-const student1 = new Student(panos);
-const student2 = new Student(nikos);
-const student3 = new Student(petros);
-
-// trainers variables
-const trainer1 = new Trainer(mantoudis);
-const trainer2 = new Trainer(mpozas);
-const trainer3 = new Trainer(xlis);
-
-// courses variables
-const courseJava = new Course(cb13Java);
-const coursePython = new Course(cb13Python);
-const courseJS = new Course(cb13JS);
-const courseCsharp = new Course(cb13Csharp);
-
-// subjects variables
-const subject1 = new Subject(html5);
-const subject2 = new Subject(css3);
-const subject3 = new Subject(oop);
-const subject4 = new Subject(security);
-
-// Assignments variables
-const assignment1Private = new Assignment(assignment1);
-assignment1Private.setSubDateTime('19:59:00 11/11/2021');
-const assignment2Private = new Assignment(assignment2);
-const assignment3Private = new Assignment(assignment3);
-const assignment4Hotel = new Assignment(assignment4);
-const assignmentGroupProject = new Assignment(groupProject);
+console.log(`\n\n[PROGRAM] =========== ============ START \n`);
 
 // Init Course JS ///////////////////////////////////////////
 
-// set end date
+// set course end date
 courseJS.setEndDate('2021/12/14');
 // add subjects to course
 courseJS.add('subjects', subject1);
@@ -83,21 +38,20 @@ courseJS.add('trainers', trainer1);
 courseJS.add('trainers', trainer2);
 courseJS.add('trainers', trainer3);
 // add assignments to course
-courseJS.add('assignments', assignment1Private);
-courseJS.add('assignments', assignment2Private);
-courseJS.add('assignments', assignment3Private);
-courseJS.add('assignments', assignment4Hotel);
-
+courseJS.add('assignments', assignment1);
+courseJS.add('assignments', assignment2);
+courseJS.add('assignments', assignment3);
+courseJS.add('assignments', assignment4);
 // remove students
-// courseJS.remove('students', student1);
+courseJS.remove('students', student1);
 // courseJS.remove('students', student2);
-courseJS.remove('students', student3);
+// courseJS.remove('students', student3);
 
 // Student /////////////////////////////////////////////
 student1.add('courses', courseJS);
-student1.add('assignments', assignment1Private);
-student1.add('assignments', assignment2Private);
-student1.add('assignments', assignment3Private);
+student1.add('assignments', assignment1);
+student1.add('assignments', assignment2);
+student1.add('assignments', assignment3);
 
 student1.submitAssignment(assignment1);
 student1.submitAssignment(assignment3);
@@ -110,15 +64,15 @@ trainer1.add('subjects', subject4);
 // trainer1.getAll('subjects', `Subject of trainer`);
 
 trainer1.giveMarks(student1, 0, 70, 80);
-// student1.getAll('assignments', 'Marked Assignment');
+trainer1.giveMarks(student1, 1, 55, 60);
+
+student1.getAll('assignments', 'Marked Assignments');
+
 // courseJS.getAll('students', 'Course JS Students: ');
-courseJS.getAll('trainers', 'Trainers: ');
-courseJS.getAll('students', 'Students: ');
-courseJS.getAll('assignments', 'Assignments: ');
+courseJS.getAll('trainers');
+courseJS.getAll('students');
+courseJS.getAll('assignments');
 
-//
-//
+// /////////////////////////////////////////////////////
 
-console.log(
-  `\n[${DateUtil.today}] PROGRAM ============ ============ ENDS \n\n`
-);
+console.log(`\n[PROGRAM] ============ ============ ENDS \n\n`);
